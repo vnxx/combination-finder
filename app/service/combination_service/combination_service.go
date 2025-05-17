9ddec0980afa393ce4ab721e5a3ae8e8d529ac79
Kevin Adam <adamkevin520@gmail.com>
2025-05-17 08:15:44 +0700

    feat(combination_service.go): optimze it, add goroutine in loop 2

--- app/service/combination_service/combination_service.go
+++ app/service/combination_service/combination_service.go
@@ -3,6 +3,8 @@ package combination_service
 import (
 	"sort"
 	"sync"
+
+	"bykevin.work/tool/combination-finder/app/util"
 )
 
 func GetCombinationResult(search float64, input []float64) [][]float64 {
@@ -10,12 +12,14 @@ func GetCombinationResult(search float64, input []float64) [][]float64 {
 	copy(sortedInput, input)
 	sort.Float64s(sortedInput)
 
+	uniqueInput := util.GetSliceUnique(sortedInput)
+
 	wg := sync.WaitGroup{}
 	mu := sync.Mutex{}
 
 	foundNumberSeries := [][]float64{}
 
-	for indexToSkip, v := range sortedInput {
+	for indexToSkip, v := range uniqueInput {
 		wg.Add(1)
 		go func(v float64, indexToSkip int) {
 			defer wg.Done()
@@ -41,6 +45,8 @@ func GetCombinationResult(search float64, input []float64) [][]float64 {
 					break
 				}
 
+				wg2 := sync.WaitGroup{}
+
 				for pointer2 := range len(sortedInput) {
 					if pointer2 == pointer {
 						continue
@@ -61,27 +67,35 @@ func GetCombinationResult(search float64, input []float64) [][]float64 {
 					expandedTempDeret2 := tempDeret2
 					expandedSumWithPointer2Value := sumWithPointer2Value
 
-					for pointer3 := range len(sortedInput) {
-						if pointer3 == pointer2 {
-							continue
+					wg2.Add(1)
+
+					go func(pointer2 int, expandedTempDeret2 []float64, expandedSumWithPointer2Value float64) {
+						defer wg2.Done()
+
+						for pointer3 := range len(sortedInput) {
+							if pointer3 == pointer2 {
+								continue
+							}
+
+							pointer3Value := sortedInput[pointer3]
+
+							expandedTempDeret2 = append(expandedTempDeret2, pointer3Value)
+							expandedSumWithPointer2Value += pointer3Value
+
+							if expandedSumWithPointer2Value > search {
+								expandedTempDeret2 = tempDeret2
+								expandedSumWithPointer2Value = sumWithPointer2Value
+								continue
+							} else if expandedSumWithPointer2Value == search {
+								found = true
+								xDeret = expandedTempDeret2
+								break
+							}
 						}
-
-						pointer3Value := sortedInput[pointer3]
-
-						expandedTempDeret2 = append(expandedTempDeret2, pointer3Value)
-						expandedSumWithPointer2Value += pointer3Value
-
-						if expandedSumWithPointer2Value > search {
-							expandedTempDeret2 = tempDeret2
-							expandedSumWithPointer2Value = sumWithPointer2Value
-							continue
-						} else if expandedSumWithPointer2Value == search {
-							found = true
-							xDeret = expandedTempDeret2
-							break
-						}
-					}
+					}(pointer2, expandedTempDeret2, expandedSumWithPointer2Value)
 				}
+
+				wg2.Wait()
 			}
 
 			if found {
